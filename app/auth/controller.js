1bd6f1ee7215958b0df4f6a7e3f8421143b610af
IdanKok <wildan.aruman90@gmail.com>
2022-09-18 15:24:35 +0700

    fix

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -1,108 +1,111 @@
 const Player = require('../player/model')
-const path = require('path')
-const fs = require('fs')
-const config = require('../../config')
+const path = require("path");
+const fs = require("fs");
+const config = require("../../config");
+
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { error } = require('console');
+module.exports = {
+    signup : async (req, res, next) => {
+        try {
+            const payload = req.body
 
-module.exports ={
-  signup : async (req, res, next)=>{
-    try {
-      const payload = req.body
+            if(req.file){
+                let tmp_path = req.file.path;
+                let originalExt =
+                  req.file.originalname.split(".")[
+                    req.file.originalname.split(".").length - 1
+                  ];
+                let filename = req.file.filename + "." + originalExt;
+                let target_path = path.resolve(
+                  config.roothPath,
+                  `public/uploads/${filename}`
+                );
+        
+                
+                const src = fs.createReadStream(tmp_path);
+                const dest = fs.createWriteStream(target_path);
+             
+        
+                src.pipe(dest);
+        
+                src.on("end", async () => {
+                  try {
+                    const player = new Player({ ...payload, avatar: filename,
+                      });
+                      await player.save();
 
-      if(req.file){
-        let tmp_path= req.file.path;
-        let originaExt = req.file.originalname.split('.')[req.file.originalname.split('.').length - 1];
-        let filename = req.file.filename + '.' + originaExt;
-        let target_path = path.resolve(config.rootPath, `public/uploads/${filename}`)
+                      delete player._doc.password
+                      res.status(201).json({ data: player}) 
+                 
+                  } catch (error) {
+                    if(error && error.name === "ValidationError"){  
+                      return  res.status(422).json({
+                        error:1,
+                        message: error.message,
+                        fields: error.errors
+                    })
 
-        const src = fs.createReadStream(tmp_path)
-        const dest = fs.createWriteStream(target_path)
+                    }
+                    next(error)
+                  } 
+                });
+            } else {
+                let player = new Player(payload)
+                await player.save()
+                delete player._doc.password
+                res.status(201).json({ data: player}) 
 
-        src.pipe(dest)
-
-        src.on('end', async ()=>{
-          try {
-
-            const player = new Player({ ...payload, avatar: filename })
-
-            await player.save();
-
-            delete player._doc.password
-
-            res.status(201).json({ data : player }) 
-          } catch (err) {
-            if(err && err.name === "ValidationError"){
-              return res.status(422).json({
-                error: 1,
-                message: err.message,
-                fields: err.errors
-              })
             }
-            next(err)
+            
+        } catch (error) {   
+            if(error && error.name === "ValidationError")
+            return  res.status(422).json({
+                error:1,
+                message: error.message,
+                fields: error.errors
+            })
+
+        }
+        next(error)
+    },
+    signin : async (req, res, next) => {
+      const { email,password } = req.body
+      Player.findOne({ email: email}).then((player) => {
+        if(player){
+          const checkPassword = bcrypt.compareSync(password, player.password)
+          if(checkPassword){
+            const token = jwt.sign({
+              player : {
+                id: player.id,
+                username: player.username,
+                name: player.name,
+                phoneNumber: player.phoneNumber,
+                avatar: player.avatar
+              }
+            }, config.jwtKey)
+             res.status(200).json({
+              data: {token}
+            })
+          } else {
+            res.status(403).json({
+              message : "Password yang anda masukan salah"
+            })
           }
-        })
-      }else {
-        let player = new Player(payload)
-
-        await player.save()
-
-        delete player._doc.password
-
-        res.status(201).json({ data : player })
-      }
-      
-    } catch (err) {
-      if(err && err.name === "ValidationError"){
-        return res.status(422).json({
-          error: 1,
-          message: err.message,
-          fields: err.errors
-        })
-      }
-      next(err)
-    }
-  },
-
-  signin : (req, res, next)=>{
-    const { email, password } = req.body
-
-    Player.findOne({ email : email }).then((player) => {
-      if(player){
-        const checkPassword = bcrypt.compareSync(password, player.password)
-        if(checkPassword){
-          const token = jwt.sign({
-            player : {
-              id : player.id,
-              username : player.username,
-              email : player.email,
-              nama : player.nama,
-              phoneNumber : player.phoneNumber,
-              avatar : player.avatar,
-            }
-          }, config.jwtKey)
-
-          res.status(200).json({
-            data: { token }
-          })
-
         } else {
           res.status(403).json({
-            message: 'password yang anda masukan salah.'
-          })  
+            message: "Email yang anda masukan tidak terdaftar"
+          })
         }
-      } else {
-        res.status(403).json({
-          message: 'email yang anda masukan belum terdaftar.'
+      }).catch((error)=>{
+        res.status(500).json({
+          message : error.message || `Internal server Error`
         })
-      }
 
-    }).catch((err)=>{
-      res.status(500).json({
-        message: err.message || `Internal server error`
-      })
+        next()
+      }) 
 
-      next()
-    })
-  }
+      
+    }
 }
