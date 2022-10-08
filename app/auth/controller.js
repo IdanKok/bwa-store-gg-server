1d07241fba1580eeae66f3c857a9c8144a7766e9
IdanKok <wildan.aruman90@gmail.com>
2022-10-09 00:32:30 +0700

    update

--- app/auth/controller.js
+++ app/auth/controller.js
@@ -76,7 +76,7 @@ module.exports ={
               id : player.id,
               username : player.username,
               email : player.email,
-              nama : player.name,
+              name : player.name,
               phoneNumber : player.phoneNumber,
               avatar : player.avatar,
             }
